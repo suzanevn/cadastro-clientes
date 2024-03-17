2548149fade08b3d960213b410f8316a93632a7b
suzanevn <suzanevn@gmail.com>
2024-03-17 17:34:07 -0300

    adicionado validacao de telefone e testes para esses metodos

--- cadastro-clientes/src/main/java/br/com/springboot/projeto/controllers/ClienteController.java
+++ cadastro-clientes/src/main/java/br/com/springboot/projeto/controllers/ClienteController.java
@@ -45,16 +45,58 @@ public class ClienteController {
     */
     @PostMapping(value = "salvar")
     @ResponseBody
-    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente){
+    public ResponseEntity<?> salvar(@RequestBody Cliente cliente){
     	try {
-            Cliente cli = clienteRepository.save(cliente);
-            return new ResponseEntity<>(cli, HttpStatus.CREATED);
+    		ResponseEntity<List<Cliente>> existe = buscarPorNome(cliente.getNome());
+    		if(existe.getBody().size() > 0)
+    			return new ResponseEntity<>("Nome ja cadastrado!", HttpStatus.BAD_REQUEST);
+    		ResponseEntity<String> retornoValida = validaClienteETelefone(cliente, false);
+    		if(retornoValida == null){
+    			Cliente cli = clienteRepository.save(cliente);
+    			return new ResponseEntity<>(cli, HttpStatus.CREATED);
+    		} else {
+    			return retornoValida;
+    		}
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
-    /**
+	private ResponseEntity<String> validaClienteETelefone(Cliente cliente, boolean atualizar) {
+//		Validar se um telefone cadastro a um cliente X não está vinculado a outro. 1 para 1.
+//		Validar se o telefone é nulo;
+//		Validar se o telefone está em um formato válido.
+		if(cliente.getNome().length() > 10)
+			return new ResponseEntity<>("O nome do cliente não pode ter mais de 10 caracteres!", HttpStatus.BAD_REQUEST);
+		boolean existeTelefone = false;
+		for (String telefone : cliente.getTelefones()) {
+			if(telefone==null || telefone.isEmpty()) {
+				return new ResponseEntity<>("Telefone não pode ter valor vazio!", HttpStatus.BAD_REQUEST);
+			}
+			if(!telefone.matches("\\(\\d{2}\\) \\d{5}-\\d{4}")) {
+				return new ResponseEntity<>("Telefone deve estar no formato (99) 99999-9999!", HttpStatus.BAD_REQUEST);
+			}
+			ResponseEntity<List<Cliente>> existe = buscarPorTelefone(telefone);
+//			ResponseEntity<List<Cliente>> existe = buscarPorTelefone(telefone, cliente.getId());
+			if(existe.getBody().size() > 0) {
+				if(atualizar) {
+					List<Cliente> clientesRetornados = existe.getBody();
+					for (Cliente clienteRetornado : clientesRetornados) {
+						if(!clienteRetornado.getId().equals(cliente.getId())) {
+							existeTelefone = true;
+							break;
+						}
+					}
+				} else {
+					existeTelefone = true;
+					break;
+				}
+			} 
+		}
+		return existeTelefone ? new ResponseEntity<>("Telefone ja cadastrado!", HttpStatus.BAD_REQUEST) : null;
+	}
+
+	/**
      * Atualiza os clientes
      * @param objeto Cliente
      * @return http status
@@ -65,8 +107,13 @@ public class ClienteController {
     	try {
     		if(cliente.getId() == null)
     			return new ResponseEntity<String>("Cliente não informado", HttpStatus.OK);
-    		Cliente user = clienteRepository.saveAndFlush(cliente);
-    		return new ResponseEntity<Cliente>(user, HttpStatus.OK);
+    		ResponseEntity<String> retornoValidaTelefone = validaClienteETelefone(cliente, true);
+    		if(retornoValidaTelefone == null) {
+	    		Cliente user = clienteRepository.save(cliente);
+	    		return new ResponseEntity<Cliente>(user, HttpStatus.OK);
+    		}else {
+    			return retornoValidaTelefone;
+    		}
     	} catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -100,7 +147,7 @@ public class ClienteController {
     		Cliente cliente = clienteRepository.findById(idCliente).get();
         	return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
     
@@ -113,11 +160,29 @@ public class ClienteController {
     @ResponseBody
     public ResponseEntity<List<Cliente>> buscarPorNome(@RequestParam(name = "name") String name){
     	try {
-    		List<Cliente> user = clienteRepository.buscaPorNome(name.trim().toUpperCase());
+    		List<Cliente> user = clienteRepository.buscarPorNome(name.trim().toUpperCase());
         	return new ResponseEntity<List<Cliente>>(user, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * Busca de telefones
+     * @param telefone e idCliente
+     * @return objeto cliente
+     */
+    @GetMapping(value = "buscarPorTelefone")
+    @ResponseBody
+    public ResponseEntity<List<Cliente>> buscarPorTelefone(@RequestParam(name = "telefone") String telefone){
+//    	public ResponseEntity<List<Cliente>> buscarPorTelefone(@RequestParam(name = "telefone") String telefone, @RequestParam(name = "idCliente") Long idCliente){
+    	try {
+//    		List<Cliente> user = clienteRepository.buscarPorTelefone(telefone.trim().toUpperCase(), idCliente);
+    		List<Cliente> clientes = clienteRepository.buscarPorTelefone(telefone.trim().toUpperCase());
+    		return new ResponseEntity<List<Cliente>>(clientes, HttpStatus.OK);
+    	} catch (Exception e) {
+    		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
     }
     
 }
--- cadastro-clientes/src/test/java/br/com/springboot/projeto/requests/ClienteControllerTest.java
+++ cadastro-clientes/src/test/java/br/com/springboot/projeto/requests/ClienteControllerTest.java
@@ -1,15 +1,18 @@
 package br.com.springboot.projeto.requests;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
@@ -17,11 +20,15 @@ import org.springframework.http.ResponseEntity;
 
 import br.com.springboot.projeto.controllers.ClienteController;
 import br.com.springboot.projeto.model.Cliente;
+import br.com.springboot.projeto.repository.ClienteRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ClienteControllerTest {
 
 	@Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
     private ClienteController clienteController;
 
     private Cliente cliente1;
@@ -54,7 +61,8 @@ public class ClienteControllerTest {
     
     @Test
     public void quandoBuscarListaDeClientesDeveRetornarTodosOsClientes() {
-        when(clienteController.listaClientes()).thenReturn(new ResponseEntity<>(clientes, HttpStatus.OK));
+//        when(clienteController.listaClientes()).thenReturn(new ResponseEntity<>(clientes, HttpStatus.OK));
+        when(clienteRepository.findAll()).thenReturn(clientes);
 
         ResponseEntity<List<Cliente>> responseEntity = clienteController.listaClientes();
 
@@ -65,19 +73,85 @@ public class ClienteControllerTest {
 
     @Test
     public void quandoSalvarDeveRetornarStatusCreated() {
-        when(clienteController.salvar(any(Cliente.class))).thenReturn(new ResponseEntity<>(cliente1, HttpStatus.CREATED));
+    	when(clienteRepository.save(cliente1)).thenReturn(cliente1);
+    	
+    	ResponseEntity<?> responseEntity = clienteController.salvar(cliente1);
 
-        ResponseEntity<Cliente> response = clienteController.salvar(cliente1);
-
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(cliente1, response.getBody());
+    	assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    	assertEquals(cliente1, responseEntity.getBody());
     }
+    
+    @Test
+    public void quandoSalvarClienteComNomeMaiorQueDezCaracteresDeveRetornarStatusBadRequest() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("NomeMaiorQue10Caracteres");
+
+        ResponseEntity<?> responseEntity = clienteController.salvar(cliente);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("O nome do cliente não pode ter mais de 10 caracteres!", responseEntity.getBody());
+    }
+    
+//    @Test
+//    public void quandoSalvarClienteComNomeJaExistenteNoBancoDeveRetornarStatusBadRequest() {
+//        Cliente cliente = new Cliente();
+//        cliente.setNome("Cliente 1");
+//        when(clienteRepository.save(cliente)).thenReturn(cliente);
+//        
+//        ResponseEntity<?> responseEntity = clienteController.salvar(cliente);
+////TODO assert msg
+//        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    	// Mock do cliente com nome maior que 10 caracteres
+//        Cliente cliente = new Cliente();
+//        cliente.setNome("Cliente 1");
+//
+//        // Chama o método salvar do clienteController
+//        ResponseEntity<?> responseEntity = clienteController.salvar(cliente);
+//
+//        // Verifica se o retorno é um status de erro (BAD_REQUEST)
+//        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+//
+//        // Verifica se a mensagem de erro está presente no corpo da resposta
+//        assertEquals("Nome ja cadastrado!", responseEntity.getBody());
+    	
+    	
+    	
+    	
+    	
+    	
+    	// Mock do cliente com nome já cadastrado
+//        Cliente cliente = new Cliente();
+//        cliente.setNome("Cliente 1");
+//
+//        // Mock do retorno da validação do cliente já cadastrado
+//        ResponseEntity<String> responseEntityValidacao = new ResponseEntity<>("Nome ja cadastrado!", HttpStatus.BAD_REQUEST);
+////        when(clienteRepository.buscarPorNome("NomeJaCad")).thenReturn(new ArrayList<>()); // Nenhum cliente encontrado, indicando que o nome não está cadastrado
+//        when(clienteController.validaCliente("Cliente 1")).thenReturn(responseEntityValidacao);
+//
+//        // Chama o método salvar do clienteController
+//        ResponseEntity<?> responseEntity = clienteController.salvar(cliente);
+//
+//        // Verifica se o retorno é um status de erro (BAD_REQUEST)
+//        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+//
+//        // Verifica se a mensagem de erro está presente no corpo da resposta
+//        assertEquals("Nome ja cadastrado!", responseEntity.getBody());
+    	
+    	//TODO
+//    	Cliente cliente = new Cliente();
+//        cliente.setNome("Cliente 1");
+//
+//        ResponseEntity<?> responseEntity = clienteController.salvar(cliente);
+//
+//        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+//        assertEquals("Nome ja cadastrado!", responseEntity.getBody());
+//    }
 
     @Test
     public void quandoAtualizarDeveRetornarStatusOk() {
     	cliente1.setEndereco("Endereco Novo");
         cliente1.setBairro("Bairro Novo");
-        when(clienteController.atualizar(cliente1)).thenReturn(new ResponseEntity<>(null,HttpStatus.OK));
+//        when(clienteRepository.saveAndFlush(cliente1)).thenReturn(null); //TODO erro mandou tirar essa linha
 
         ResponseEntity<?> response = clienteController.atualizar(cliente1);
 
@@ -86,8 +160,8 @@ public class ClienteControllerTest {
     }
     
     @Test
-    public void quandoBuscarClientePorIdDeveRetornarSomenteOsTelefonesDoClienteInformado() {
-        when(clienteController.buscarClienteId(1L)).thenReturn(new ResponseEntity<>(cliente1, HttpStatus.OK));
+    public void quandoBuscarClientePorIdDeveRetornarSomenteOClienteInformado() {
+    	when(clienteRepository.findById(1L)).thenReturn(Optional.of(cliente1));
 
         ResponseEntity<Cliente> responseEntity = clienteController.buscarClienteId(1L);
 
@@ -98,13 +172,80 @@ public class ClienteControllerTest {
     }
     
     @Test
+    public void quandoBuscarClientePorIdNaoExistenteDeveRetornarStatusNotFound() {
+        when(clienteRepository.findById(3L)).thenReturn(Optional.empty());
+
+        ResponseEntity<Cliente> responseEntity = clienteController.buscarClienteId(3L);
+
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    }
+    
+    @Test
     public void quandoBuscarClientePorNomeDeveRetornarTodosOsClienteComNomeInformado() {
-        when(clienteController.buscarPorNome("Cliente")).thenReturn(new ResponseEntity<>(clientes, HttpStatus.OK));
+        when(clienteRepository.buscarPorNome("Cliente")).thenReturn(clientes);
 
-        ResponseEntity<List<Cliente>> responseEntity = clienteController.buscarPorNome("Cliente");
+        List<Cliente> resultado = clienteRepository.buscarPorNome("Cliente");
 
-        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
-        assertEquals(responseEntity.getBody(), clientes);
+        assertEquals(2, resultado.size());
+        assertEquals("Cliente 1", resultado.get(0).getNome());
+        assertEquals("Cliente 2", resultado.get(1).getNome());
+    }
+    
+    @Test
+    public void quandoDeletarClientePorIdDeveRetornarStatusOK() {
+        ResponseEntity<String> responseEntity = clienteController.delete(1L);
+
+        verify(clienteRepository, times(1)).deleteById(1L);
+        
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+    
+    @Test
+    public void quandoAtualizarClienteComTelefoneInvalidoDeveRetornarBadRequest() {
+        Cliente cliente = new Cliente();
+        cliente.setId(1L);
+        cliente.setNome("invalido");
+        List<String> telefones = new ArrayList<String>();
+        telefones.add("1234567890");
+        cliente.setTelefones(telefones); // Telefone inválido
+
+        // Simular que o cliente já existe no banco de dados
+//        when(clienteRepository.save(cliente)).thenReturn(cliente);//TODO erro mandou tirar essa linha
+
+        // Chamar o método atualizar do controlador
+        ResponseEntity<?> responseEntity = clienteController.atualizar(cliente);
+
+        // Verificar se o resultado é um BadRequest
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+    
+    @Test
+    public void quandoAtualizarClienteComTelefoneJaCadastrado_DeveRetornarBadRequest() {
+        Cliente clienteExistente = new Cliente();
+        clienteExistente.setId(2L); // ID de outro cliente
+        clienteExistente.setNome("Maria");
+        List<String> telefones = new ArrayList<String>();
+        telefones.add("(11) 98888-1234");
+        clienteExistente.setTelefones(telefones); // Telefone que já está cadastrado
+
+        // Configurar o cliente que está sendo atualizado
+        Cliente clienteAtualizado = new Cliente();
+        clienteAtualizado.setId(1L);
+        clienteAtualizado.setNome("João");
+        clienteAtualizado.setTelefones(telefones); // Mesmo telefone que já está cadastrado
+
+        List<Cliente> clientesTelefone = new ArrayList<Cliente>();
+        clientesTelefone.add(clienteExistente);
+        // Simular que o cliente com o mesmo telefone já está cadastrado no banco de dados
+//        when(clienteController.buscarPorTelefone("1234567890")).thenReturn(new ResponseEntity<List<Cliente>>(clientesTelefone, HttpStatus.OK));
+        when(clienteRepository.buscarPorTelefone("(11) 98888-1234")).thenReturn(clientesTelefone);
+
+        // Chamar o método atualizar do controlador
+        ResponseEntity<?> responseEntity = clienteController.atualizar(clienteAtualizado);
+
+        // Verificar se o resultado é um BadRequest
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("Telefone ja cadastrado!", responseEntity.getBody());
     }
     
     private List<String> adicionaDoisTelefones() {
